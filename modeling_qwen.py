f58581913a14cfb9d0dd0e1e6a1a44c800a3556e
shaoping <260259423@qq.com>
2024-03-26 16:23:01 +0800

    Update modeling_qwen.py

--- modeling_qwen.py
+++ modeling_qwen.py
@@ -967,6 +967,77 @@ class QWenLMHeadModel(QWenPreTrainedModel):
 
         return response, history
 
+    def chat_batch(
+        self,
+        tokenizer: PreTrainedTokenizer,
+        query: Optional[List[str]],
+        history: Optional[HistoryType],
+        system: str = "You are a helpful assistant.",
+        append_history: bool = True,
+        stream: Optional[bool] = _SENTINEL,
+        stop_words_ids: Optional[List[List[int]]] = None,
+        generation_config: Optional[GenerationConfig] = None,
+        **kwargs,
+    ) -> Tuple[str, HistoryType]:
+        generation_config = generation_config if generation_config is not None else self.generation_config
+
+        assert stream is _SENTINEL, _ERROR_STREAM_IN_CHAT
+        assert generation_config.chat_format == 'chatml', _ERROR_BAD_CHAT_FORMAT
+        if history is None:
+            history = []
+        if stop_words_ids is None:
+            stop_words_ids = []
+
+        max_window_size = kwargs.get('max_window_size', None)
+        if max_window_size is None:
+            max_window_size = generation_config.max_window_size
+        assert query is not None
+        raw_text_list = []
+        context_tokens_list = []
+        for q in query:
+            raw_text, context_tokens = make_context(
+                tokenizer,
+                q,
+                history=history,
+                system=system,
+                max_window_size=max_window_size,
+                chat_format=generation_config.chat_format,
+            )
+            raw_text_list.append(raw_text)
+            context_tokens_list.append(context_tokens)
+
+
+        stop_words_ids.extend(get_stop_words_ids(
+            generation_config.chat_format, tokenizer
+        ))
+        input_ids = torch.tensor(context_tokens_list).to(self.device)
+        outputs = self.generate(
+                    input_ids,
+                    stop_words_ids=stop_words_ids,
+                    return_dict_in_generate=False,
+                    generation_config=generation_config,
+                    **kwargs,
+                )
+        response_list = []
+        for idx, ele in enumerate(outputs):
+            raw_text = raw_text_list[idx]
+            context_tokens = context_tokens_list[idx]
+            response = decode_tokens(
+                ele,
+                tokenizer,
+                raw_text_len=len(raw_text),
+                context_length=len(context_tokens),
+                chat_format=generation_config.chat_format,
+                verbose=False,
+                errors='replace'
+            )
+            response_list.append(response)
+
+        #if append_history:
+        #    history.append((query, response))
+
+        return response_list
+
     def chat_stream(
             self,
             tokenizer: PreTrainedTokenizer,
